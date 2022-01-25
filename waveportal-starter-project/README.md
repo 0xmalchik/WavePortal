daa7931a3759baaf8e6540f73f472ec934fbee21
0xmalchik <jianthony108@gmail.com>
2022-01-25 15:38:19 +0100

    first commit

---
+++ waveportal-starter-project/README.md
@@ -0,0 +1,12 @@
+# buildspace Wave Portal 👋 
+
+### **Welcome!**
+To get started with this project, follow these commands:
+
+1. Run `npm install` at the root of your directory
+2. Run `npm run start` to start the project
+3. Start forking !
+
+
+### **Live Demo**
+https://waveportal-starter-project-antoinersw.vercel.app/
---
+++ waveportal-starter-project/build/precache-manifest.02fa2a63b4df8aa8082808fcbc439653.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5c64b85a5ea57aff1cf5409683c647",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6ba5fe374a8f9d79dda",
+    "url": "/static/css/main.e46d3f62.chunk.css"
+  },
+  {
+    "revision": "128821e6dc40d3cad728",
+    "url": "/static/js/2.267eb7a3.chunk.js"
+  },
+  {
+    "revision": "a6ba5fe374a8f9d79dda",
+    "url": "/static/js/main.23af6064.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
